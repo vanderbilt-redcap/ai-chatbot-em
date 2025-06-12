d1ac5e18d03da079b33a84529ec4a08bf52ca27b
Vaishali Jagtap <71736051+vaishali-vumc@users.noreply.github.com>
2025-06-12 18:56:11 -0400

    Debug

--- Classes/OpenAIFiles.php
+++ Classes/OpenAIFiles.php
@@ -26,6 +26,7 @@ class OpenAIFiles
             }
             foreach ($docIds as $docId) {
                 $fileAttr = \Files::getEdocContentsAttributes($docId);
+                echo "after getedocs"; die;
                 $curlFile = new \CURLStringFile($fileAttr[2], $fileAttr[1], $fileAttr[0]);
                 $headers = [
                     'Content-Type: multipart/form-data',
