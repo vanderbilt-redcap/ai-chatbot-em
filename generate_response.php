480a632fcff78c34af36a13e92b4272625aab7a6
Vaishali Jagtap <71736051+vaishali-vumc@users.noreply.github.com>
2025-06-12 21:05:16 -0400

    Debug

--- generate_response.php
+++ generate_response.php
@@ -136,7 +136,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'generate') {
                 'purpose' => 'assistants',
                 'file' => $curlFile,
             ];
-
+echo "--".PROXY_HOSTNAME; die;
             $resFileUpload = Api::curlAPIPost($api_key, $endpoint . "files?api-version=" . $api_version, $data, $headers);
             echo "after call"; die;
             $fileIds[] = $resFileUpload['id'];
